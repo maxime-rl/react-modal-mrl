458f53a38a5d0174ce05c1ba9318f44152a72703
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-12-11 03:51:02 +0100

    feat: add accessibility and handle keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,66 @@
 import React, { useState } from "react";
 import { Modal } from "./lib";
+import closeIcon from "./assets/close-icon.svg";
 import "./App.css";
 
 /**
- * Modal component rendering example
+ * Modal component rendering
+ * Minimal and custom example available
  * @returns {ReactElement}
  */
 function App() {
-  const [modal, setModal] = useState(false);
-  const trigger = () => setModal(!modal);
+  const [basicModal, setBasicModal] = useState(false);
+  const [customModal, setCustomModal] = useState(false);
+
+  const triggerBasicModal = () => setBasicModal(!basicModal);
+  const triggerCustomModal = () => setCustomModal(!customModal);
 
   return (
     <>
-      <button className="click-me" onClick={() => trigger()}>
-        Click me
+      <button className="trigger-modal" onClick={() => triggerBasicModal()}>
+        Basic modal
       </button>
-      <Modal show={modal} close={trigger} title="Dynamic title">
-        <p>Modal content with children</p>
-        <p>Child</p>
-        <p>Child</p>
+      <Modal
+        show={basicModal}
+        close={triggerBasicModal}
+        closeIcon={closeIcon}
+        title="Basic modal"
+      ></Modal>
+      <button className="trigger-modal" onClick={() => triggerCustomModal()}>
+        Custom modal
+      </button>
+      <Modal
+        show={customModal}
+        close={triggerCustomModal}
+        closeIcon={closeIcon}
+        title="Custom modal"
+      >
+        <p className="modal__subtitle">
+          Example of modal content with children and icon
+        </p>
+        <div className="modal__form">
+          <input
+            className="modal__form-input modal__form-input--first-name"
+            type="text"
+            placeholder="First name"
+          />
+          <input
+            className="modal__form-input modal__form-input--last-name"
+            type="text"
+            placeholder="Last name"
+          />
+          <input
+            className="modal__form-input modal__form-input--zip-code"
+            type="number"
+            placeholder="Zip code"
+          />
+          <input
+            className="modal__form-input modal__form-input--department"
+            type="text"
+            placeholder="Department"
+          />
+          <button className="modal__form-button">Submit</button>
+        </div>
       </Modal>
     </>
   );
--- src/lib/Modal/Modal.jsx
+++ src/lib/Modal/Modal.jsx
@@ -11,29 +11,68 @@ import styles from "./Modal.module.css";
  * @returns {ReactElement}
  */
 export default function Modal({ show, close, closeIcon, title, children }) {
+  const modalRef = React.useRef();
+
+  const keyDownHandler = (e) => {
+    if (e.key !== "Tab") return;
+
+    const focusableModalElements = modalRef.current.querySelectorAll(
+      "a[href], button:not([disabled]), textarea, input, select"
+    );
+
+    const firstElement = focusableModalElements[0];
+    const lastElement =
+      focusableModalElements[focusableModalElements.length - 1];
+
+    if (!e.shiftKey && document.activeElement === lastElement) {
+      firstElement.focus();
+      return e.preventDefault();
+    }
+
+    if (e.shiftKey && document.activeElement === firstElement) {
+      lastElement.focus();
+      e.preventDefault();
+    }
+  };
+
+  const keyListenersMap = new Map([
+    [27, close],
+    [9, keyDownHandler],
+  ]);
+
+  React.useEffect(() => {
+    function keyListener(e) {
+      const listener = keyListenersMap.get(e.keyCode);
+      return listener && listener(e);
+    }
+    document.addEventListener("keydown", keyListener);
+
+    return () => document.removeEventListener("keydown", keyListener);
+  });
+
   return (
-    <>
+    <div
+      className={`${styles.overlay} ${show ? styles.show : ""} `}
+      onClick={() => close()}
+      role="dialog"
+      aria-modal="true"
+    >
       <div
-        className={`${styles.overlay} ${show ? styles.show : ""} `}
-        onClick={() => close()}
+        className={styles.content}
+        onClick={(e) => e.stopPropagation()}
+        ref={modalRef}
       >
-        <div className={styles.content} onClick={(e) => e.stopPropagation()}>
-          {title ? <h2 className={styles.title}>{title}</h2> : null}
-          <button className={styles.close} onClick={() => close()}>
-            {closeIcon ? (
-              <img
-                className={styles.closeContent}
-                src={closeIcon}
-                alt="close"
-              />
-            ) : (
-              <span className={styles.closeContent}>❌</span>
-            )}
-          </button>
-          {children ? <div className={styles.body}>{children}</div> : null}
-        </div>
+        {title ? <h2 className={styles.title}>{title}</h2> : null}
+        <button className={styles.close} onClick={() => close()}>
+          {closeIcon ? (
+            <img className={styles.closeContent} src={closeIcon} alt="close" />
+          ) : (
+            <span className={styles.closeContent}>❌</span>
+          )}
+        </button>
+        {children ? <div>{children}</div> : null}
       </div>
-    </>
+    </div>
   );
 }
 
